52c5f18b6b2141e6eb17b1b583287966370aa704
parvathimpillai@gmail.com <parvathimpillai@gmail.com>
2025-07-03 14:16:02 +0530

    advanceed search with number

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -22,7 +22,7 @@ class EmployeeController extends Controller
     public function getEmployees(Request $request)
     {
         try {
-            $response = Http::get('http://localhost:8002/api/v1/employee-data');
+            $response = Http::get('http://localhost:8000/api/v1/employee-data');
 
             if ($response->successful()) {
                 $employees = $response->json(); // ✅ FIXED HERE
@@ -40,17 +40,17 @@ class EmployeeController extends Controller
             ], 500);
         }
     }
-    // public function employeeShow($id)
-    // {
-    //     try {
-    //         $response = Http::get("https://jsonplaceholder.typicode.com/users/{$id}");
-    //         if ($response->successful()) {
-    //             $employee = $response->json();
-    //             return view('employees.show', compact('employee'));
-    //         }
-    //         return view('employees.show', ['error' => 'Employee not found']);
-    //     } catch (\Exception $e) {
-    //         return view('employees.show', ['error' => 'An error occurred: ' . $e->getMessage()]);
-    //     }
-    // }
+    public function employeeShow($id)
+    {
+        try {
+            $response = Http::get("http://localhost:8000/api/v1/employee-data/{$id}");
+            if ($response->successful()) {
+                $employee = $response->json();
+                return view('employees.show', compact('employee'));
+            }
+            return view('employees.show', ['error' => 'Employee not found']);
+        } catch (\Exception $e) {
+            return view('employees.show', ['error' => 'An error occurred: ' . $e->getMessage()]);
+        }
+    }
 }
