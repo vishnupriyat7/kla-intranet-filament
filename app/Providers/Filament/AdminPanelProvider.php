e8c004ce94cda95b1ef998a7ebbb2258e883211f
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2025-07-15 16:51:22 +0530

    title length less than 50 filter applied-also save title length whileediting and create one done

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -44,7 +44,6 @@ class AdminPanelProvider extends PanelProvider
             ->favicon(asset('images/favicon.png'))
             ->font('Poppins')
             ->maxContentWidth('full') // Set content width to full
-            ->viteTheme(['resources/css/filament.css', 'resources/js/filament.js'])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
